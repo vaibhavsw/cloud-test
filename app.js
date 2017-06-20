5195ef945334501b9547daae7d8d454e5b20da12
Vaibhav Swarnkar <Vaibhav Swarnkar>
2017-06-20 16:50:53 +0530

    New Cloud Test Project for test

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+app.get('/', function (request, response) {
+    response.sendFile('./index.html');
+});
+
+app.listen(9000, function () {
+  console.log('Example app listening on port 9000!')
+})
