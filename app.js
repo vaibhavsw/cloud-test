826e221cee2f704ef00c3dacbe51f5d0018a2b04
vaibhavswarnkar <vaibhavconnected@gmail.com>
2017-07-07 19:09:10 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-app.get('/', function (request, response) {
-    response.sendFile('./index.html');
-});
+// app.get('/', function (request, response) {
+//     response.sendFile('./index.html');
+// });
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!')
