6b13587f4efec2e56909e32f5a7d24c17c54a89a
Vaibhav Swarnkar <Vaibhav Swarnkar>
2017-07-12 18:42:28 +0530

    Working HTML and Loader file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-
-app.get('/', function (request, response) {
-    response.sendFile('./index.html');
+app.get('/loaderio-9ee702e98ed483284f79947eaa5afae2/', function (request, response) {
+    response.sendFile('loaderio-9ee702e98ed483284f79947eaa5afae2.txt',{root:__dirname});
 });
 
-app.listen(9000, function () {
-  console.log('Example app listening on port 9000!')
+app.get('/', function (request, response) {
+    response.sendFile('index.html',{root:__dirname});
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
